7480f646be79d0b8d7a63d6401a3e3fc79a08d38
Gineude Silvestre de Carvalho <c1294114@interno.bb.com.br>
2020-09-11 09:03:10 -0300

    Primeiro projeto do curso de flutter cod3r

---
+++ lib/dados.dart
@@ -0,0 +1,29 @@
+final perguntas = const [
+  {
+    "texto": "Qual é a sua cor favorita?",
+    "respostas": [
+      {"texto": "Preto", "pontuacao": 10},
+      {"texto": "Vermelho", "pontuacao": 5},
+      {"texto": "Verde", "pontuacao": 3},
+      {"texto": "Branco", "pontuacao": 1}
+    ]
+  },
+  {
+    "texto": "Qual é a seu animal favorito?",
+    "respostas": [
+      {"texto": "Coelho", "pontuacao": 10},
+      {"texto": "Cobra", "pontuacao": 5},
+      {"texto": "Elefante", "pontuacao": 3},
+      {"texto": "Leão", "pontuacao": 1},
+    ]
+  },
+  {
+    "texto": "Qual é o seu instrutor favorito?",
+    "respostas": [
+      {"texto": "Leo", "pontuacao": 10},
+      {"texto": "Maria", "pontuacao": 5},
+      {"texto": "João", "pontuacao": 3},
+      {"texto": "Pedro", "pontuacao": 1},
+    ]
+  }
+];
